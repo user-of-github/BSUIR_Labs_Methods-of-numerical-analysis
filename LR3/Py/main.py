5abec05618160b2cc2028684c41adff40ebd80c6
Nikita <nikitosminsk100@gmail.com>
2022-03-14 18:18:17 +0300

    Report for LR3 - allmost ready

--- LR3/Py/main.py
+++ LR3/Py/main.py
@@ -1,8 +1,9 @@
 from test_run import test_run
+import data
 
 
 def main() -> None:
-    test_run()
+    test_run(data.TEST_EQUATION[3], -1, 1, data.ERROR)
 
 
 if __name__ == '__main__':
--- LR3/Py/test_run.py
+++ LR3/Py/test_run.py
@@ -1,24 +1,23 @@
-import data
 from half_division_method import get_root_by_half_division
 from chord_method import get_root_by_chord_method
 from sturm_method import get_number_of_roots_by_sturm_theorem
 from newton_method import get_root_by_newton_method
 
 
-def test_run() -> None:
-    number_of_roots: int = get_number_of_roots_by_sturm_theorem(data.TEST_EQUATION, data.RANGE_FROM, data.RANGE_TO)
+def test_run(equation: list, range_from: float, range_to: float, error: float) -> None:
+    number_of_roots: int = get_number_of_roots_by_sturm_theorem(equation, range_from, range_to)
 
-    print(f'According Sturm\'s theorem there are {number_of_roots} roots on range [{data.RANGE_FROM} .. {data.RANGE_TO}]\n')
+    print(f'According Sturm\'s theorem there are {number_of_roots} roots on range [{range_from} .. {range_to}]\n')
 
     # The following values are taken from chart (Maple)
-    look_for_from: float = 0
-    look_for_to: float = 2
+    look_for_from: float = -0.8
+    look_for_to: float = -0.05
 
-    root, iterations_count = get_root_by_half_division(data.TEST_EQUATION, look_for_from, look_for_to, data.ERROR)
+    root, iterations_count = get_root_by_half_division(equation, look_for_from, look_for_to, error)
     print(f'[Half-division-method]: Got x = {root} | Needed {iterations_count} iterations')
 
-    root, iterations_count = get_root_by_chord_method(data.TEST_EQUATION, look_for_from, look_for_to, data.ERROR)
+    root, iterations_count = get_root_by_chord_method(equation, look_for_from, look_for_to, error)
     print(f'[Chord-method]: Got x = {root} | Needed {iterations_count} iterations')
 
-    root, iterations_count = get_root_by_newton_method(data.TEST_EQUATION, look_for_from, look_for_to, data.ERROR)
+    root, iterations_count = get_root_by_newton_method(equation, look_for_from, look_for_to, error)
     print(f'[Newton-method]: Got x = {root} | Needed {iterations_count} iterations')
