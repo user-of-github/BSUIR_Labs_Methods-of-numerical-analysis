54e2f8d2485d37153f940c665f2e524deddd221c
Nikita <nikitosminsk100@gmail.com>
2022-05-24 19:29:59 +0300

    LR8 ready

--- 4_semester/LR8/data.py
+++ 4_semester/LR8/data.py
@@ -2,9 +2,9 @@ import math
 
 ACCURACY_DERIVATIVE: float = 0.01
 
-FUNCTIONS: list = [lambda x: math.log10(x), lambda x: math.atan(x)]
-ACCURACY_INTEGRALS: list[float] = [0.000001, 0.000001]
+FUNCTIONS: list = [lambda x: math.log10(x), lambda x: math.atan(x), lambda x: math.cosh(x)]
+ACCURACY_INTEGRALS: list[float] = [0.000001, 0.000001, 0.000001]
 
-INTERVALS: list[tuple[float, float]] = [(1, 3), (0, 2)]
+INTERVALS: list[tuple[float, float]] = [(1, 3), (0, 2), (0, 2)]
 
 OPTIONS_COUNT: int = min(len(FUNCTIONS), len(ACCURACY_INTEGRALS), len(INTERVALS))
